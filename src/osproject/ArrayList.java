59f7d146b3240378f3e35e4b58653663e628ad9f
SulizLina <alzeghabil@gmail.com>
2024-04-06 01:12:37 +0300

    First commit

---
+++ src/osproject/ArrayList.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package osproject;
+
+/**
+ *
+ * @author alzeghaibi.
+ */
+public class ArrayList<T> {
+
+    class Node<D> {
+
+        public D data;
+        public Node<D> next;
+
+        public Node(D data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node<T> head;
+    int count;
+
+    public ArrayList() {
+        head = null;
+        count = 0;
+    }
+
+    public void add(T e) {
+        addLast(e);
+    }
+
+    public void addLast(T e) {
+        Node<T> node = new Node<T>(e);
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+
+            node.next = temp.next;
+            temp.next = node;
+        }
+
+        count++;
+    }
+
+    public T remove() throws IndexOutOfBoundsException {
+        return removeFirst();
+    }
+
+    public T removeFirst() throws IndexOutOfBoundsException {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("The list is empty.");
+        }
+
+        Node<T> node = head;
+
+        if (head.next == null) {
+            head = null;
+        } else {
+            head = head.next;
+            node.next = null;
+        }
+
+        count--;
+
+        return node.data;
+    }
+
+    public T get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= count) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            return head.data;
+        } else {
+            Node<T> node = head;
+
+            for (int i = 1; i <= index; i++) {
+                node = node.next;
+            }
+
+            return node.data;
+        }
+    }
+
+    public void set(int index, T e) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= count) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            head.data = e;
+        } else {
+            Node<T> node = head;
+
+            for (int i = 1; i <= index; i++) {
+                node = node.next;
+            }
+
+            node.data = e;
+        }
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return (count == 0);
+    }
+}
---
+++ src/osproject/Process.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package osproject;
+
+/**
+ *
+ * @author alzeghaibi.
+ */
+public class Process {
+
+    public int id;
+    public int priority;
+    public int arrivalTime;
+    public int burstTime;
+    public int startTime;
+    public int terminateTime;
+    public int turnAroundTime;
+    public int waitingTime;
+    public int responseTime;
+    public int timeInCPU;
+
+    public Process(int id, int arrivalTime, int burstTime, int priority) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.priority = priority;
+        this.burstTime = burstTime;
+        this.startTime = 0;
+        this.terminateTime = 0;
+        this.turnAroundTime = 0;
+        this.waitingTime = 0;
+        this.responseTime = 0;
+        this.timeInCPU = 0;
+    }
+
+    public String toString() {
+        return "  ID: P" + id
+                + "\n  Priority: " + priority
+                + "\n  CPU burst: " + burstTime
+                + "\n  Arrival Time: " + arrivalTime
+                + "\n  Start Time: " + startTime
+                + "\n  Termination Time: " + terminateTime
+                + "\n  Turn around Time: " + turnAroundTime
+                + "\n  Waiting Time: " + waitingTime
+                + "\n  Response Time: " + responseTime;
+    }
+}
